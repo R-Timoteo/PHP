1411f618654fa28a76f78cfe7c7bfe34597ba9e3
RTimoteo <rafael.tmtrodrigues@gmail.com>
2023-10-02 23:30:33 -0300

    Please enter the commit message for your changes. Lines starting

---
+++ ex003/index.php
@@ -0,0 +1,11 @@
+<?php
+  $nome = "Rafael";
+  $sobrenome = "Timoteo \u{1F437}";
+
+  echo "$nome $sobrenome ";
+
+  const CANAL = "Eu adoro o curso \u{1F437}";
+
+  echo CANAL . "em video ";
+
+  echo CANAL . " " . date( 'Y');
