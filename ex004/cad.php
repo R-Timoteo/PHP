f43d0672162cabc78831009a2fa7565bab727edd
RTimoteo <rafael.tmtrodrigues@gmail.com>
2023-10-09 22:38:20 -0300

    add cad.php

---
+++ ex004/cad.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>
+            Resultado do Processamento
+        </h1>
+    </header>
+    <main>
+        <?php 
+        $n = $_GET["nome"];
+        $s = $_GET["sobrenome"];
+        echo " <p> E um prazer te conhecer <strong> $n $s </strong> sua Gostosa do Caralho"
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a pagina anterior</a></p>
+    </main>
+    
+</body>
+</html>
